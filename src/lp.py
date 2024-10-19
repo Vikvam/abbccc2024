05c4b04049cedf3523a5165a387299413aed93da
Vikvam <vikvam@disroot.org>
2024-10-19 11:20:31 +0200

    Final

--- src/lp.py
+++ src/lp.py
@@ -24,14 +24,9 @@ class Result:
     H: np.array
     P: np.array
     load: np.array
-    buffered: np.array
-    buffer_capacity: np.array
-    production_gap: np.array
-    P_delta: float
-    P_avg: float
 
     def __str__(self):
-        return f"obj: {self.obj}, buffer_capacity: {self.buffer_capacity}, P_delta: {self.P_delta}"
+        return f"obj: {self.obj}, "
 
 
 class LP:
@@ -55,16 +50,13 @@ class LP:
         # variables
         l = model.addVars(self.N, self.T, vtype=grb.GRB.CONTINUOUS, lb=0, ub=1, name="l")       # system output, current load
         L = model.addVars(self.N, self.T, vtype=grb.GRB.CONTINUOUS, lb=0, ub=1, name="L")       # system input, desired load
-        H = model.addVars(self.T, vtype=grb.GRB.CONTINUOUS, lb=0, name="H")                     # amount of H2
-        b = model.addVar(vtype=grb.GRB.CONTINUOUS, lb=0, ub=self.b_max, name="b")                              # buffer capacity
-        B = model.addVars(self.T, vtype=grb.GRB.CONTINUOUS, name="B")                           # rate of charging
+        H = model.addVars(self.T, vtype=grb.GRB.CONTINUOUS, lb=0, name="H")                       # rate of charging
+        # N = model.addVars(self.N, self.T, vtype=grb.GRB.BINARY, name="N")
         # auxiliary variables
         on = model.addVars(self.N, self.T, vtype=grb.GRB.BINARY)                                # helper, to limit load
         P_out = model.addVars(self.T, lb=0, vtype=grb.GRB.CONTINUOUS)                           # helper, consumption
         P_delta_ = model.addVars(self.T, vtype=grb.GRB.CONTINUOUS, name="P_delta")
         P_delta = model.addVars(self.T, vtype=grb.GRB.CONTINUOUS)
-        P_avg_ = model.addVars(self.T, vtype=grb.GRB.CONTINUOUS)
-        P_avg = model.addVars(self.T, vtype=grb.GRB.CONTINUOUS, name="P_avg")
 
         # load in <load_min, 1>
         for t in range(self.T):
@@ -82,10 +74,6 @@ class LP:
             H[t] == grb.quicksum(l[n,t] * self.p_max[n] * self.H[n] for n in range(self.N))
             for t in range(self.T)
         ))
-        # buffer capacity
-        model.addConstr(b <= self.b_max)
-        model.addConstrs((B[t] <= b for t in range(self.T)))
-        model.addConstrs((B[t] >= -b for t in range(self.T)))
         # power in /out
         model.addConstrs((
             P_out[t] == grb.quicksum(l[n,t] * self.p_max[n] for n in range(self.N))
@@ -93,33 +81,22 @@ class LP:
         ))
         # power difference
         model.addConstrs((
-            P_delta_[t] == self.P[t] - B[t] - P_out[t]
+            P_delta_[t] == self.P[t] - P_out[t]
             for t in range(self.T)
         ))
         model.addConstrs((
             P_delta[t] == grb.abs_(P_delta_[t])
             for t in range(self.T)
         ))
-        # power average over T
-        avg = np.mean(self.P[t])
-        model.addConstrs((
-            P_avg_[t] == avg - P_out[t]
-            for t in range(self.T)
-        ))
-        model.addConstrs((
-            P_avg[t] == grb.abs_(P_avg_[t])
-            for t in range(self.T)
-        ))
+        # iniclude in sol
+        # for n in range(self.N):
+        #     model.addGenConstrIndicator(grb.quicksum(on[n,t] for t in range(self.T)), 1, L[n, t] >= self.l_min[n])
+        #     model.addGenConstrIndicator(on[n, t], 0, L[n, t] == 0)
 
-        # stability constraints
-        # model.addConstr(grb.quicksum(P_delta) <= 10e5)
-        # model.addConstr(grb.quicksum(P_avg) <= 10e5)
 
         model.setObjectiveN(
             - grb.quicksum(H) / (np.sum(self.p_max) + self.T) * 1
-            + grb.quicksum(P_avg[t] for t in range(self.T)) / avg
         , index=0)
-        model.setObjectiveN(b, index=1)
         model.ModelSense = grb.GRB.MINIMIZE
 
         model.Params.NumericFocus = 3
@@ -138,18 +115,11 @@ class LP:
             load = [[self.model.getVarByName(f"l[{n},{t}]").x for t in range(self.T)] for n in range(self.N)]
             P = [[self.model.getVarByName(f"l[{n},{t}]").x * self.p_max[n] for t in range(self.T)] for n in range(self.N)]
             H = [self.model.getVarByName(f"H[{t}]").x for t in range(self.T)]
-            B = [self.model.getVarByName(f"B[{t}]").x for t in range(self.T)]
-            P_avg = [self.model.getVarByName(f"P_avg[{t}]").x for t in range(self.T)]
             result = Result(
                 obj=-np.sum(self.model.obj),
                 H=np.array(H),
                 P=np.array(P),
                 load=np.array(load),
-                buffered=np.array(B),
-                buffer_capacity=self.model.getVarByName(f"b").x,
-                P_delta=None,
-                P_avg=np.array(P_avg),
-                production_gap=[]
             )
             self.validate_result(result)
             return result
